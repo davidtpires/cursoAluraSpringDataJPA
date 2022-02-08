eb0513a003808c5ca5ccadbcac5bde238782c1b3
David Trindade <davidt.info@gmail.com>
2022-02-07 21:16:37 -0300

    Visualizar e deletar o registro

--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -18,27 +18,36 @@ public class CrudCargoService {
 	}
 
 	public void inicial(Scanner scanner) {
-		
+
 		while (system) {
 			System.out.println("Qual acao de cargo deseja executar");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Salvar");
 			System.out.println("2 - Atualizar");
-			
+			System.out.println("3 - Visualizar");
+			System.out.println("4 - Deletar");
+
 			int action = scanner.nextInt();
-			
+
 			switch (action) {
 			case 1:
 				salvar(scanner);
 				break;
 			case 2:
-				atualizar(scanner);			
+				atualizar(scanner);
+				break;
+			case 3:
+				visualizar();
+				break;
+			case 4:
+				deletar(scanner);
+				break;
 			default:
 				system = false;
 				break;
 			}
 		}
-		
+
 	}
 
 	private void salvar(Scanner scanner) {
@@ -61,6 +70,18 @@ public class CrudCargoService {
 		cargo.setDescricao(descricao);
 		cargoRepository.save(cargo);
 		System.out.println("Atualizado");
+	}
+
+	private void visualizar() {
+		Iterable<Cargo> cargos = cargoRepository.findAll();
+		cargos.forEach(cargo -> System.out.println(cargo));
+	}
+	
+	private void deletar(Scanner scanner) {
+		System.out.println("Id");
+		int id = scanner.nextInt();
+		cargoRepository.deleteById(id);
+		System.out.println("Deletado");
 	}
 
 }
