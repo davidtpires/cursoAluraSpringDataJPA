cd747d583169fe0a01dbfb4087de230f76b67ceb
David Trindade <davidt.info@gmail.com>
2022-02-07 22:12:52 -0300

    Cadastrando entidades Funcionario e UnidadeTrabalho

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -5,18 +5,30 @@ import java.util.Scanner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
-	
-	private final CrudCargoService cargoService;
-	
+
 	private Boolean system = true;
 
-	public SpringDataApplication(CrudCargoService cargoService) {
+	private final CrudCargoService cargoService;
+
+	private final CrudFuncionarioService funcionarioService;
+
+	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+
+	public SpringDataApplication(CrudCargoService cargoService,
+			CrudFuncionarioService funcionarioService, 
+			CrudUnidadeTrabalhoService unidadeTrabalhoService) {
 		this.cargoService = cargoService;
+		this.funcionarioService = funcionarioService;
+		this.unidadeTrabalhoService = unidadeTrabalhoService;
 	}
 
 	public static void main(String[] args) {
@@ -26,19 +38,31 @@ public class SpringDataApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Scanner scanner = new Scanner(System.in);
-		
-		while(system) {
-			System.out.println("Qual acao voce quer executar");
+
+		while (system) {
+			System.out.println("Qual função deseja executar?");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Cargo");
+			System.out.println("2 - Funcionario");
+			System.out.println("3 - Unidade");
 			
-			int action = scanner.nextInt();
-			if(action == 1) {
-				cargoService.inicial(scanner);
-			} else {
-				system = false;
+			Integer function = scanner.nextInt();
+
+			switch (function) {
+				case 1:
+					cargoService.inicial(scanner);
+					break;
+				case 2:
+					funcionarioService.inicial(scanner);
+					break;
+				case 3:
+					unidadeTrabalhoService.inicial(scanner);
+					break;
+				default:
+					System.out.println("Finalizando");
+					system = false;
+					break;
 			}
 		}
 	}
-
 }
--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -9,26 +9,25 @@ import br.com.alura.spring.data.repository.CargoRepository;
 
 @Service
 public class CrudCargoService {
-
+	
 	private Boolean system = true;
 	private final CargoRepository cargoRepository;
-
+	
 	public CrudCargoService(CargoRepository cargoRepository) {
 		this.cargoRepository = cargoRepository;
 	}
-
+	
 	public void inicial(Scanner scanner) {
-
-		while (system) {
+		while(system) {
 			System.out.println("Qual acao de cargo deseja executar");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Salvar");
 			System.out.println("2 - Atualizar");
 			System.out.println("3 - Visualizar");
 			System.out.println("4 - Deletar");
-
+			
 			int action = scanner.nextInt();
-
+			
 			switch (action) {
 			case 1:
 				salvar(scanner);
@@ -46,10 +45,11 @@ public class CrudCargoService {
 				system = false;
 				break;
 			}
+			
 		}
-
+		
 	}
-
+	
 	private void salvar(Scanner scanner) {
 		System.out.println("Descricao do cargo");
 		String descricao = scanner.next();
@@ -58,20 +58,20 @@ public class CrudCargoService {
 		cargoRepository.save(cargo);
 		System.out.println("Salvo");
 	}
-
+	
 	private void atualizar(Scanner scanner) {
 		System.out.println("Id");
 		int id = scanner.nextInt();
 		System.out.println("Descricao do Cargo");
 		String descricao = scanner.next();
-
+		
 		Cargo cargo = new Cargo();
 		cargo.setId(id);
 		cargo.setDescricao(descricao);
 		cargoRepository.save(cargo);
 		System.out.println("Atualizado");
 	}
-
+	
 	private void visualizar() {
 		Iterable<Cargo> cargos = cargoRepository.findAll();
 		cargos.forEach(cargo -> System.out.println(cargo));
@@ -83,5 +83,5 @@ public class CrudCargoService {
 		cargoRepository.deleteById(id);
 		System.out.println("Deletado");
 	}
-
+	
 }
